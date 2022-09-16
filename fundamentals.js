ad9c5b2e540890643a80dcdde734d05a7ac8d728
Ilija Gacov <20093475@wit.ie>
2022-09-16 15:19:36 +0100

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -5,7 +5,15 @@ const aCar = {
     model: 'Corolla',
     c: 1.8
   },
-  
+  milage: 10000,
+  colour: {
+    extColour: 'red',
+    interior: {
+      intColour: 'brown',
+      texture: 'leather',
+      shade: 'cream'
+    },
+  },
   registration : {year: 201, county: "WD", number: 1058}
 };
 
@@ -17,3 +25,6 @@ console.log("Reg. = " +
   aCar.registration.number 
 
 );
+console.log('It is a '+ aCar.colour.extColour + " car, " + aCar.milage +
+             " milage, with " + aCar.colour.interior.texture + " interior."
+        )
